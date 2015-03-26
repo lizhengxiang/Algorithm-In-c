1e7d4e1cb9e540fe67d3c8091365814339d67b11
lizhengxiang <1065482100@qq.com>
2015-03-26 18:28:20 +0800

    A questionable recursive program

---
+++ questionable_recursive.c
@@ -0,0 +1,22 @@
+/*
+*If the argument N is odd, this function calls itself with 3N + 1 as an argument; if N is even,
+*it calls itself with N/2 as an argument. We cannot use induction to prove that this program
+*terminates, because not every recursive call uses an argument smaller than the one given.
+*/
+
+#include<stdio.h>
+int puzzle(int N){
+	if(N == 1)
+		return 1;
+	if(N % 2 == 0)
+		return puzzle(N/2);
+	else
+		return puzzle(3*N+1);
+}
+int main(){
+	int i;
+	for(i = 20; i < 200000; i *= 10){
+		printf("%d\t%d\n", i, puzzle(i));
+	}
+	return 0;
+}
