8a5ee75ea02954fd4669aae357a37c3cccf73052
lizhengxiang <1065482100@qq.com>
2015-04-05 10:34:24 +0800

    Array implementation

--- Item_implementation.h
+++ Item_implementation.h
@@ -1,3 +1,9 @@
+/*
+This code implements the overloaded operator< and the functions scan, rand, and
+show that are declared in Program 6.9. Since the records are small structures, we can have
+exch use the built-in assignment operator without undue worry about the expense of
+copying items.
+*/
 #include"Item.h"
 #include<stdio.h>
 #include<stdlib.h>
