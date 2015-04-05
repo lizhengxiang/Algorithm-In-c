9e983b363a0c832682c44c3e1e5949921e169ae9
lizhengxiang <1065482100@qq.com>
2015-04-05 10:10:00 +0800

    Item

--- Item.h
+++ Item.h
@@ -1,8 +1,16 @@
-typedef int Item;
-int eq(float a, float b){
-	//printf("%0.2f\n", fabs(a-b));
-	if(fabs(a-b) < 0.000001)
-		return 1;
-	else
-		return 0;
-}
+/*
+The file Item.h that is included in Program 6.9 defines the data type for the items to be
+sorted. In this example, the items are small records consisting of integer keys and
+associated floating-point information. We declare that the overloaded operator< will be
+implemented separately, as will the three functions scan (read an Item into its argument),
+rand (store a random Item in its argument), and show (print an Item).
+*/
+typedef double Item;
+#define key (a)  (a)
+#define less(a, b) (key(a) < key(b))
+#define exch(a, b){ Item t = a; a = b; b = t}
+#define compexch(a, b) if(less(a, b)) exch(a, b)
+
+Item ITEMrand(void);
+int ITEMscan(Item *);
+void ITEMshow(Item );
